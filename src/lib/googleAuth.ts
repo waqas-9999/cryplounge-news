4a760f4ffc232681422ba59d8fa3923b9d3d4edb
Muhammad Waqas <waqasshahab1999@gmail.com>
2026-08-23 15:46:26 +0500

    Update googleAuth.ts

--- src/lib/googleAuth.ts
+++ src/lib/googleAuth.ts
@@ -1,7 +1,11 @@
 import { GoogleAuth } from "google-auth-library";
 
+const credentials = JSON.parse(
+  process.env.GOOGLE_SERVICE_ACCOUNT_JSON!
+);
+
 export const googleAuth = new GoogleAuth({
-  keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+  credentials,
   scopes: [
     "https://www.googleapis.com/auth/cloud-platform",
   ],
